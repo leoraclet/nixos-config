6cb60ed93deb53f65adee33ca865e276e7810012
leonne <leonne@email.com>
2025-07-13 21:38:28 +0200

    Fixed Documents partition mounting permissions

--- system/modules/system.nix
+++ system/modules/system.nix
@@ -2,6 +2,6 @@
   fileSystems."/home/leonne/Documents" = {
     device = "/dev/disk/by-uuid/26923F4A923F1E2D";
     fsType = "ntfs-3g";
-    options = ["rw" "nofail" "uid=1000" "gid=100" "dmask=001" "fmask=111"];
+    options = ["rw" "nofail" "uid=1000" "gid=100" "dmask=017" "fmask=017"];
   };
 }
