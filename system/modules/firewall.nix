078cc5387777d8747d35464b59b9b03109bd1f13
Léo Raclet <leo.raclet@gmail.com>
2025-07-28 16:21:23 +0200

    2025-07-28 16:21:15

--- system/modules/firewall.nix
+++ system/modules/firewall.nix
@@ -1,6 +1,6 @@
 {...}: {
   # Open ports in the firewall.
-  networking.firewall.enable = true;
+  networking.firewall.enable = false;
   # networking.firewall.allowedTCPPorts = [ 3000 ];
   # networking.firewall.allowedUDPPorts = [ 3000 ];
   # Or disable the firewall altogether.
