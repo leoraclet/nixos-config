4e98eca71dfe4ebd9c324dfca34e611d844ab7df
N£utr0nys <110538448+leoraclet@users.noreply.github.com>
2025-11-13 14:55:04 -0500

    2025-11-13 14:54:51

--- system/modules/networking.nix
+++ system/modules/networking.nix
@@ -16,7 +16,7 @@
       hostsFile = builtins.fetchurl {
         url = hostsPath;
         # nix-prefetch-url --type sha256 "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"
-        sha256 = "1qaz48jcqpfbjsll3b7ca66a8lkjzhxd80jx3yry1y8r9i0hqn5y";
+        sha256 = "17q811bfchzg8iir0df2wga2kvc0xknpmqrarkjdpj3y5h6pp72h";
       };
     in
       builtins.readFile "${hostsFile}";
