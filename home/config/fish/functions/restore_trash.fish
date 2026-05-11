21d43a67fdf9411429049ed511cef84e2bb3c39c
N£utr0nys <110538448+leoraclet@users.noreply.github.com>
2026-05-11 09:11:01 -0400

    Changed fish config

--- home/config/fish/functions/restore_trash.fish
+++ home/config/fish/functions/restore_trash.fish
@@ -1,9 +1,11 @@
 function restore_trash
     trash list \
+    | sort --numeric-sort \
     | fzf --multi \
     | awk '{$1=$1;print}' \
     | rev \
     | cut -d ' ' -f1 \
     | rev \
     | xargs trash restore --match=exact --force
+    commandline -f repaint
 end
